6a7f131995414aa01e2df523334d24a50095ab25
Rob Howard <rob@zinclusive.com>
2025-11-27 23:11:51 -0700

    Save

--- main.py
+++ main.py
@@ -25,9 +25,13 @@ def get_transcript(video_url):
 def get_transcript_string(video_url):
     def func():
         j = get_transcript(video_url)
-        s = " ".join([item['text'] for item in j]).replace(".", "\n")
+        s = " ".join([item['text'] for item in j]).strip().replace(".", ".\n")
+        s = [x.strip() for x in s.splitlines("\n")]
+        s = "\n".join(s)
         return s
-    return get_json_cache(f"transcript_{tools.md5(video_url)}.json", func)
+    fn = f"cache/transcripts/transcript_{tools.md5(video_url)}.json"
+    return get_json_cache(fn, func)
+
 
 def all_videos(channel_url):
     options = {
@@ -72,14 +76,14 @@ def all_videos(channel_url):
             yield video
 
 
-def compile_transcripts(videos):
+def compile_transcripts(channel_url):
     fn = tools.clean_filename(channel_url)
     fn = f"cache/{fn}.json"
     videos = tools.readJson(fn)
     with open(f"cache/output.txt", "w") as f:
         for video in videos['entries']:
             fn = video['id']
-            fn = f"cache/transcripts/{fn}.json"
+            fn = f"cache/videos/{fn}.json"
             if not os.path.exists(fn):
                 continue
             v = tools.readJson(fn)
@@ -99,14 +103,60 @@ def pull_transcripts(channel_url):
             sleep(random.randint(30, 60))
             return v
         fn = video['id']
-        fn = f"cache/transcripts/{fn}.json"
+        fn = f"cache/videos/{fn}.json"
         if os.path.exists(fn):
             continue
         print(video['title'])
         v = get_json_cache(fn, func)
         pass
 
+
+def pull_transcript(video_url):
+    fn = video_url.split("v=")[1]
+    fnBase = f"cache/videos/{fn}"
+    fn = f"{fnBase}.json"
+    if os.path.exists(fn):
+        return
+    print(video_url)
+
+    def func():
+        options = {
+            'quiet': True,
+            'extract_flat': True,        # True = Faster, gets metadata without downloading
+            'skip_download': True,
+            'force_generic_extractor': False,
+        }
+
+        with yt_dlp.YoutubeDL(options) as ydl:
+            v = {}
+            v = ydl.extract_info(video_url, download=False)
+            v['transcript'] = get_transcript_string(video_url)
+            tools.writeText(f"{fnBase}.txt", f"""
+Id: {v['id']}
+Title: {v['title']}
+Category: {', '.join(v.get('categories', []))}
+Tags: {', '.join(v.get('tags', []))}
+Transcript: {v['transcript']}
+""".strip())
+            return v
+        
+    v = get_json_cache(fn, func)
+
+
 if __name__ == "__main__":
-    channel_url = "https://www.youtube.com/@PrometheanAction/videos"
-    pull_transcripts(channel_url)
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--pull", help="Pull transcripts from url")
+    parser.add_argument("-c", "--channel", help="Youtube channel url")
+    parser.add_argument("-v", "--video", help="A single video url")
+
+    args = parser.parse_args()
+    if args.pull:
+        url = args.pull
+        if "youtube.com/" in url and "/watch" in url:
+            pull_transcript(url)
+        else:
+            pull_transcripts(url)
+    else:
+        raise ValueError("Please provide a channel URL or a single video URL with the --channel or --video parameter")
     #compile_transcripts(channel_url)
